95716bb9fd60b1449c844a23ce2fa79ae2b1477b
Expat131 <120243175+Expat131@users.noreply.github.com>
2022-12-10 10:58:03 +0300

    Add files via upload

---
+++ ЛР1_Palindrome.java
@@ -0,0 +1,32 @@
+// Класс Palindrome определяет является ли слово палиндромом
+public class Palindrome {
+	public static void main(String[] args) {
+		// Цикл перебирает слова переданные в аргумент args и вызывает функцию isPalindrome
+		// Если возвращаемое значение true - слово палиндром
+		// false - слово не палиндром
+		for (int i = 0; i < args.length; i++) {
+			String s = args[i];
+			if (isPalindrome(s)) {
+				System.out.println(s + " палиндром");
+			}
+			else{
+				System.out.println(s + " не палиндром");
+			}
+		}
+
+	}
+
+	// функция возвращает true/false с помощью метода equals определяется, равна ли передананная строка, строке возвращаемой из функции reverseString
+	public  static  boolean  isPalindrome(String  s){
+		return s.equals(reverseString(s));
+	}ууууууууу
+
+	// Функция проходит от конца длинны строки посимвольно до 0, и переписывает символы в новую строку и возвращает это значение, тем самым переварачиваем строку
+	public static String reverseString(String s){
+		String reverseS = "";
+		for (int i = s.length() - 1; i >= 0; i--) {
+			reverseS = reverseS + s.charAt(i);
+		}
+		return reverseS;
+	}
+}
